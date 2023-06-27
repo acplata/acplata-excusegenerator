88f2db526754124cd9f0d6d144320379370eac36
acplata <133425402+acplata@users.noreply.github.com>
2023-06-27 00:19:00 +0000

    se borro

--- src/app.js
+++ src/app.js
@@ -9,12 +9,8 @@ function excuseGenerator(array){
   const getExcuseGenerator = Math.floor(Math.random() * array.length);
   return array[getExcuseGenerator];
 }
-
-const randomWho = excuseGenerator(who);
-const randomAction = excuseGenerator(action);
-const randomWhat = excuseGenerator(what);
-const randomWhen = excuseGenerator(when);
-console.log(randomWho,randomAction,randomWhat,randomWhen)
-
-const excuse = ` ${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
-parrafo.innerText = excuse;
+ const randomWho = excuseGenerator(who);
+ const randomAction = excuseGenerator(action);
+ const randomWhat = excuseGenerator(what);
+ const randomWhen = excuseGenerator(when);
+ console.log(randomWho,randomAction,randomWhat,randomWhen);
